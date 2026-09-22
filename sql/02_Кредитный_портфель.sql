2f9f2d176b90ea592463ed637363c4f779c1656c
FM-DA <gradalis123@gmail.com>
2026-09-22 15:09:44 +0600

    02_Кредитный_портфель.sql

--- sql/02_Кредитный_портфель.sql
+++ sql/02_Кредитный_портфель.sql
@@ -66,8 +66,8 @@ base AS (
     JOIN first_credit fc
         ON fc.credit_hub_id = c.credit_hub_id
     WHERE c.product_id NOT IN (1258,1244,1238,1248,1241,1291,1237,1240,1210,1242,1239,1289,1206,1223,1288,1207,1224,1205,1269)
-        AND LOWER(ci.date_range) >= DATE '2025-07-01'
-        AND LOWER(ci.date_range) < DATE '2026-07-01'
+        AND LOWER(ci.date_range) >= DATE '{START_DATE}'
+        AND LOWER(ci.date_range) < DATE '{END_DATE}'
 )
 SELECT
     customer_id AS "ID клиента",
